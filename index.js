afbc922b570becf2e9c616ff0eeed5a485a15d97
Emmanuel Atama <emmanuelatama@outlook.com>
2021-07-12 17:50:39 +0100

    SWITZERLAND - bigmacPriceIndex1

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-/*
-Index is calculated by :
-  - Get current US Big Mac price
-  - Get current Big Mac price from country we're comparing
-  - Divide US Big Mac price by country's Big Mac price
-    (this is called the implied purchasing power)
-  - Get country's currency rate to USD value
-  - Substract currency rate with the implied purchasing power
-  - Divide the previous result by the implied purchasing power
-  - This will give you the Big Mac Index
-  - Make sure you search for updated values!
+// SWITZERLAND - bigmacPriceIndex1
 
-For example, using figures in July 2008:
-  - the price of a Big Mac was $3.57 in the United States 
-  - the price of a Big Mac was £2.29 in the United Kingdom 
-  - the implied purchasing power parity was $1.56 to £1, that is $3.57/£2.29 = 1.56
-  - this compares with an actual exchange rate of $2.00 to £1 at the time
-  - (2.00 – 1.56) / 1.56 = 28%
-  - the pound was thus overvalued against the dollar by 28%
-*/
+let usBigmacPrice = 5.66;
+let swissBigmacPrice = 6.5;
+let impliedPurchasingPower = usBigmacPrice / swissBigmacPrice;
+// Annual exchange rate of $1 to 0.92chf at the time of this evaluation.
+let dollarExchangeRate = 1;
+let swissExchangeRate = dollarExchangeRate - 0.08;
+console.log(swissExchangeRate);
+let swissBigmacPriceIndex =
+  (dollarExchangeRate - impliedPurchasingPower) / impliedPurchasingPower;
+console.log(swissBigmacPriceIndex);
+let bigmacPriceIndex1 = (swissBigmacPriceIndex * 100).toFixed();
+console.log(bigmacPriceIndex1);
+console.log(bigmacPriceIndex1 + "%");
+
+// SWEDEN - bigmacPriceIndex2
+
+let usBigmacPrice = 5.66;
+let swissBigmacPrice = 6.5;
+let impliedPurchasingPower = usBigmacPrice / swissBigmacPrice;
+// Annual exchange rate of $1 to 0.92chf at the time of this evaluation.
+let dollarExchangeRate = 1;
+let swissExchangeRate = dollarExchangeRate - 0.08;
+console.log(swissExchangeRate);
+let swissBigmacPriceIndex =
+  (dollarExchangeRate - impliedPurchasingPower) / impliedPurchasingPower;
+console.log(swissBigmacPriceIndex);
+let bigmacPriceIndex1 = (swissBigmacPriceIndex * 100).toFixed();
+console.log(bigmacPriceIndex1);
+console.log(bigmacPriceIndex1 + "%");
